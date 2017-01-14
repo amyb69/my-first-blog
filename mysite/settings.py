9898a1ed5161dfbcdbee16257913f4576d3f51a8
amyb69 <emeline.bedoucha@gmail.com>
2017-01-14 10:23:56 +0100

    allowed host modification

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nvoac%ro-idj8cktgfc1@8dz78yp-z(o8*c%%1f5i@kxh1zw*c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'amyb69.pythonanywhere.com']
 
 
 # Application definition
